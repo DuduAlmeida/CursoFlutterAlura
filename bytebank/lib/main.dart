aef0813cae162b617f8b664cddb500910280cb95
Eduardo Almeida <contato.eduardopex@hotmail.com>
2020-12-02 19:12:11 -0300

    feat(main): added a better writing

--- bytebank/lib/main.dart
+++ bytebank/lib/main.dart
@@ -2,24 +2,7 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
       home: Scaffold(
-        body: Column(
-          children: [
-            Card(
-              child: ListTile(
-                leading: Icon(Icons.monetization_on),
-                title: Text('100.00'),
-                subtitle: Text('10000'),
-              ),
-            ),
-            Card(
-              child: ListTile(
-                leading: Icon(Icons.monetization_on),
-                title: Text('200.00'),
-                subtitle: Text('10000'),
-              ),
-            ),
-          ],
-        ),
+        body: ListaTransferencia(),
         appBar: AppBar(
           title: Text('Transferências'),
         ),
@@ -28,3 +11,56 @@ void main() => runApp(MaterialApp(
         ),
       ),
     ));
+
+class ListaTransferencia extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        ItemTransferancia('100.00', '1000'),
+        ItemTransferancia('200.00', '1000'),
+        ItemTransferancia('300.00', '1000'),
+      ],
+    );
+  }
+}
+
+/*** 
+ * O Widget que contém um item de determinada transferência
+ */
+class ItemTransferancia extends StatelessWidget {
+  /* #region Inputs Properties*/
+
+  /*** 
+   * O valor da transferência
+   */
+  final String valor;
+
+  /*** 
+   * O número da conta à transferir o valor
+   */
+  final String numeroConta;
+
+  /* #Endregion Inputs Properties*/
+
+  /* #region Constructor*/
+
+  ItemTransferancia(this.valor, this.numeroConta);
+
+  /* #Endregion Constructor*/
+
+  /* #region Build*/
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        leading: Icon(Icons.monetization_on),
+        title: Text(valor),
+        subtitle: Text(numeroConta),
+      ),
+    );
+  }
+
+  /* #Endregion Build*/
+}
