f8fcdcebc56b4be462ecc4fcfae984413bd256ca
Eduardo Almeida <contato.eduardopex@hotmail.com>
2020-12-02 18:24:04 -0300

    feat(main): started to build page application

--- bytebank/lib/main.dart
+++ bytebank/lib/main.dart
@@ -1,8 +1,30 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(Text(
-    'Bem vindo ao Bytebank',
-    textDirection: TextDirection.ltr,
-  ));
-}
+void main() => runApp(MaterialApp(
+      home: Scaffold(
+        body: Column(
+          children: [
+            Card(
+              child: ListTile(
+                leading: Icon(Icons.monetization_on),
+                title: Text('100.00'),
+                subtitle: Text('10000'),
+              ),
+            ),
+            Card(
+              child: ListTile(
+                leading: Icon(Icons.monetization_on),
+                title: Text('200.00'),
+                subtitle: Text('10000'),
+              ),
+            ),
+          ],
+        ),
+        appBar: AppBar(
+          title: Text('Transferências'),
+        ),
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add),
+        ),
+      ),
+    ));
