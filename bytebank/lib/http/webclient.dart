f1535c3126a5645e29e162c39501eb6ba8a27704
Eduardo Almeida <contato.eduardopex@hotmail.com>
2020-12-15 09:14:46 -0300

    Convertendo json para objeto do Dart

--- bytebank/lib/http/webclient.dart
+++ bytebank/lib/http/webclient.dart
@@ -1,14 +1,34 @@
 /// #region Imports
 
+import 'dart:convert';
+import 'package:bytebank/models/contacts.dart';
+import 'package:bytebank/models/transaction.dart';
 import 'package:http/http.dart';
 import 'package:http_interceptor/http_interceptor.dart';
 
 /// #endregion Imports
 
-Future<void> findAll() async {
+Future<List<Transaction> findAll() async {
   final Client client =
       HttpClientWithInterceptor.build(interceptors: [LoggingInterceptor()]);
   final Response response = await client.get('localhost:8080/transactions');
+  final List<dynamic> decodedJson = jsonDecode(response.body);
+  final List<Transaction> transactions = new List();
+
+  for (Map<String, dynamic> transactionJson in decodedJson) {
+    final Map<String, dynamic> contactJson = transactionJson['contact'];
+    final Transaction transaction = Transaction(
+      transactionJson['value'],
+      Contact(
+        0,
+        contactJson['name'],
+        contactJson['accountNumber'],
+      ),
+    );
+    transactions.add(transaction);
+  }
+
+  return transactions;
 }
 
 class LoggingInterceptor implements InterceptorContract {
