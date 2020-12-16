cb53088ba17002baf152a7b2bd4e07c769d12cfb
Eduardo Almeida <contato.eduardopex@hotmail.com>
2020-12-16 14:05:07 -0300

    Plano de fundo

--- lojinha_alura/lib/pages/detalhes_page.dart
+++ lojinha_alura/lib/pages/detalhes_page.dart
@@ -1,24 +1,51 @@
 /// #region Imports
 
 import 'package:flutter/material.dart';
+import 'package:lojinha_alura/models/movel.dart';
 import 'package:lojinha_alura/widgets/appbar_custom.dart';
 
 /// #endregion Imports
 
 class DetalhesPage extends StatelessWidget {
-  const DetalhesPage({Key key}) : super(key: key);
+  /// #region Constructor
+
+  DetalhesPage({Key key, @required this.movel}) : super(key: key);
+
+  /// #endregion Constructor
+
+  /// #region Public Propeties
+
+  final Movel movel;
+
+  /// #endregion Public Propeties
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBarCustom(
-        titulo: 'Detalhes',
+    return Container(
+      ///
+      /// Plano de fundo é definido aqui
+      ///
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage('utilidades/assets/imagens/${movel.foto}'),
+          fit: BoxFit.cover,
+        ),
       ),
-      body: FlatButton(
-        onPressed: () {
-          Navigator.of(context).pushNamed('/carrinho');
-        },
-        child: Text('Agora vamos para a página de carrinho'),
+
+      ///
+      /// Conteúdo da página
+      ///
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        appBar: AppBarCustom(
+          titulo: '',
+        ),
+        body: FlatButton(
+          onPressed: () {
+            Navigator.of(context).pushNamed('/carrinho');
+          },
+          child: Text('Agora vamos para a página de carrinho'),
+        ),
       ),
     );
   }
--- lojinha_alura/lib/widgets/element_grid_produtos.dart
+++ lojinha_alura/lib/widgets/element_grid_produtos.dart
@@ -2,6 +2,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:lojinha_alura/models/movel.dart';
+import 'package:lojinha_alura/pages/detalhes_page.dart';
 import 'package:lojinha_alura/widgets/degrade_elementos_grid_produto.dart';
 import 'package:lojinha_alura/widgets/imagem_elementos_grid_produto.dart';
 import 'package:lojinha_alura/widgets/titulo_elementos_grid_produto.dart';
@@ -28,57 +29,66 @@ class ElementoGridProdutos extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.all(10),
-      decoration: BoxDecoration(
-        ///
-        /// Recebe uma lista de BoxShadow
-        ///
-        boxShadow: [
-          BoxShadow(
-            ///
-            /// O raio de até onde essa sombra vai alcançar
-            ///
-            spreadRadius: 2,
-
-            ///
-            /// O quanto a sombra será desfocada
-            ///
-            blurRadius: 8,
-
-            ///
-            /// A cor da sombra
-            ///
-            color: Colors.black12,
-
-            ///
-            /// O movimento da sombra, sendo o primeiro relativo ao eixo x e o segundo ao y
-            ///
-            offset: Offset(0, 0),
+    return InkWell(
+      onTap: () {
+        Navigator.of(context).push(
+          MaterialPageRoute(
+            builder: (context) => DetalhesPage(movel: movel),
           ),
-        ],
-      ),
+        );
+      },
+      child: Container(
+        margin: EdgeInsets.all(10),
+        decoration: BoxDecoration(
+          ///
+          /// Recebe uma lista de BoxShadow
+          ///
+          boxShadow: [
+            BoxShadow(
+              ///
+              /// O raio de até onde essa sombra vai alcançar
+              ///
+              spreadRadius: 2,
 
-      ///
-      /// Vai cortar os elementos filhos para ficarem da forma deseja,
-      /// como se tivesse um overflow com estilo
-      ///
-      child: ClipRRect(
-        borderRadius: BorderRadius.circular(8),
-        child: Stack(
-          ///
-          /// Define o alinhamento das propriedades filhas
-          ///
-          alignment: Alignment.center,
-          children: [
-            ImagemElementoGridProdutos(
-              imagem: movel.foto,
-            ),
-            DegradeElementoGridProdutos(),
-            TituloElementoGridProdutos(
-              titulo: movel.titulo,
+              ///
+              /// O quanto a sombra será desfocada
+              ///
+              blurRadius: 8,
+
+              ///
+              /// A cor da sombra
+              ///
+              color: Colors.black12,
+
+              ///
+              /// O movimento da sombra, sendo o primeiro relativo ao eixo x e o segundo ao y
+              ///
+              offset: Offset(0, 0),
             ),
           ],
+        ),
+
+        ///
+        /// Vai cortar os elementos filhos para ficarem da forma deseja,
+        /// como se tivesse um overflow com estilo
+        ///
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(8),
+          child: Stack(
+            ///
+            /// Define o alinhamento das propriedades filhas
+            ///
+            alignment: Alignment.center,
+            children: [
+              ImagemElementoGridProdutos(
+                imagem: movel.foto,
+              ),
+              DegradeElementoGridProdutos(),
+              TituloElementoGridProdutos(
+                titulo: movel.titulo,
+              ),
+            ],
+          ),
         ),
       ),
     );
